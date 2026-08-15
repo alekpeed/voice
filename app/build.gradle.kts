18e3f29324edc0d1ae276cc1bea324efaeda6579
alekpeed <77586388+alekpeed@users.noreply.github.com>
2026-08-15 15:51:29 -0400

    Align Android JVM targets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,10 @@ android {
         vectorDrawables.useSupportLibrary = true
     }
     buildFeatures { compose = true; buildConfig = true }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
     packaging { resources.excludes += "/META-INF/{AL2.0,LGPL2.1}" }
     testOptions { unitTests.isIncludeAndroidResources = true }
 }
